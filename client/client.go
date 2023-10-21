ad0187722dfdd93f21f6400fda2a8b13f7b4d4e1
vladsendrix <vladsendrix@gmail.com>
2023-10-21 14:16:29 +0300

    add a golang client

---
+++ client/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:6969")
+	if err != nil {
+		fmt.Println("Error connecting to the server:", err)
+		return
+	}
+	defer conn.Close()
+
+	go receiveMessages(conn)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		scanner.Scan()
+		msg := scanner.Text()
+		if msg == "exit" {
+			break
+		}
+		_, err := conn.Write([]byte(msg + "\n"))
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			break
+		}
+	}
+}
+
+func receiveMessages(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Connection closed by the server.")
+			break
+		}
+		fmt.Print(msg)
+	}
+}
